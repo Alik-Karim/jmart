df810c364d81adcc773d0a7a2075b074ca8600ff
Alik-Karim <abdul.malik91@ui.ac.id>
2021-09-18 10:56:30 +0700

    CS 2

--- jmart/AbdulMalikKarimAJmartMR/Product.java
+++ jmart/AbdulMalikKarimAJmartMR/Product.java
@@ -12,14 +12,15 @@ public class Product
     private static int idCounter;
     public int id;
     public String name;
+    public int weight;
     public boolean conditionUsed;
     public PriceTag priceTag;
     public ProductRating rating;
     
     public Product (String name,int weight,boolean conditionUsed, PriceTag priceTag){
-        this.idCounter = idConter++;
+        this.idCounter = idCounter++;
         this.name = name;
-        this.weight = wight;
+        this.weight = weight;
         this.conditionUsed = conditionUsed;
         this.priceTag = priceTag;
     }
