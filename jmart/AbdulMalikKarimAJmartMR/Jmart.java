f9cc84d650bd1ed7f1e50107b01ceb135eb6ae84
Alik-Karim <abdul.malik91@ui.ac.id>
2021-11-05 22:48:25 +0700

    tp modul 6

--- jmart/AbdulMalikKarimAJmartMR/Jmart.java
+++ jmart/AbdulMalikKarimAJmartMR/Jmart.java
@@ -1,15 +1,39 @@
 package AbdulMalikKarimAJmartMR;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 class Jmart {
+	
+	class Country{
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
     public static void main(String[] args) {
-        System.out.println(Shipment.Duration.KARGO.getEstimatedArrival(new Date()));
-        Account account = new Account(212," Acong"," aslabnetlab@gmail.com "," OOPsusahbet ");
-        account.validate();
-        
-        System.out.println("Hello from Eclipe!");
+        String filepath = "D:\\download\\OOP prak\\jmart\\jmart\\AbdulMalikKarimAJmartMR/city.json";
+        Gson gson = new Gson();
+        try
+        {
+        	BufferedReader br = new BufferedReader(new FileReader(filepath));
+        	Country input = gson.fromJson(br, Country.class);
+        	System.out.println("name: " + input.name);
+        	System.out.println("population: " + input.population);
+        	System.out.println("states:");
+        	input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e)
+        {
+    	e.printStackTrace();
     }
+   }
 }
