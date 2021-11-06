58190e8468f6751e079615358a87952b37efc049
Alik-Karim <abdul.malik91@ui.ac.id>
2021-11-06 11:16:38 +0700

    cs modul 6

--- jmart/AbdulMalikKarimAJmartMR/Product.java
+++ jmart/AbdulMalikKarimAJmartMR/Product.java
@@ -2,43 +2,34 @@ package AbdulMalikKarimAJmartMR;
 
 public class Product extends Serializable
 {
-    private static int idCounter = 0;
+    public int accountId;
     public String name;
     public int weight;
     public boolean conditionUsed;
-    public Treasury priceTag;
-    public ProductCategory category ;
-    public ProductRating rating;
-    public int storeId;
-    public Shipment.MultiDuration multiDuration;
+    public double discount;
+    public ProductCategory category;
+    public double price;
+    public byte shipmentPlans;
 
-    public Product(int id, int storeId, String name, 
-    int weight, boolean conditionUsed, Treasury priceTag,
-    ProductCategory category, Shipment.MultiDuration multiDuration){
-        super(id);
-        this.storeId = storeId;
+    public Product(int accountId, String name, int weight, boolean conditionUsed, double price, double discount, ProductCategory category, byte shipmentPlans) {
+        this.accountId = accountId;
         this.name = name;
         this.weight = weight;
         this.conditionUsed = conditionUsed;
-        this.priceTag = priceTag;
+        this.price = price;
+        this.discount = discount;
         this.category = category;
-        this.rating = new ProductRating();
-        this.multiDuration = multiDuration;
+        this.shipmentPlans = shipmentPlans;
     }
 
-    public boolean read(String content){
-        return false;
+    @Override
+    public String toString(){
+        return "Name: " + this.name +
+                "\nWeight: " + this.weight +
+                "\nconditionUsed: " + this.conditionUsed +
+                "\npriceTag: " + this.price +
+                "\ncategory: " + this.category +
+                "\nrating: " + this.discount +
+                "\nstoreId: " + this.accountId;
     }
-
-    public String toString() {
-        return 
-        "Name: " + this.name +
-        "\nWeight: " + this.weight +
-        "\nconditionUsed: " + this.conditionUsed +
-        "\npriceTag: " + this.priceTag +
-        "\ncategory: " + this.category +
-        "\nrating: " + this.rating +
-        "\nstoreId: " + this.storeId;
-    }
-    
 }
